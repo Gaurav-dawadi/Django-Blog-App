85ff3836e3f8b516878815faf9c326d00269d08c
Gaurav-dawadi <gaurabdawadi@gamil.com>
2020-12-25 21:32:57 +0545

    feat: add views for user registration and change login required permissions

--- main/views.py
+++ main/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, redirect, reverse
-from django.contrib.auth import authenticate, login
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from .forms import PostForm, LoginForm
-from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy, reverse
+from .forms import PostForm, LoginForm, RegistrationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+# from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView,
+                                    DeleteView)
 
 # Create your views here.
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
 
 def user_login(request):
     form = LoginForm()
@@ -19,6 +29,7 @@ def user_login(request):
 
         if user is not None:
             login(request, user)
+            messages.success(request, 'Successfully Logged In')
             return redirect('blog:home')
         else:
             messages.error(request, 'Credentials doesnot match OR User doesnot exists')
@@ -40,6 +51,7 @@ class HomeView(CheckRequestUser, ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'posts'
+    ordering = '-id'
 
 
 class PostCreateView(CheckRequestUser, CreateView):
@@ -47,6 +59,11 @@ class PostCreateView(CheckRequestUser, CreateView):
     form_class = PostForm
     template_name = 'create.html'
     success_url = reverse_lazy('blog:home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Blog was successfully created')
+        return super().form_valid(form)
 
 
 class PostDetailView(CheckRequestUser, DetailView):
@@ -62,6 +79,7 @@ class PostUpdateView(CheckRequestUser, UserPassesTestMixin, UpdateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Blog sucessfully updated')
         return super().form_valid(form)
 
     def test_func(self):
@@ -72,9 +90,30 @@ class PostUpdateView(CheckRequestUser, UserPassesTestMixin, UpdateView):
 
     def get_success_url(self, *args, **kwargs):
         post = self.get_object()
-        return reverse('blog:detail', args=[post.slug])     
+        return reverse('blog:detail', args=[post.slug])    
 
 
+class PostDeleteView(CheckRequestUser, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+
+    def get_success_url(self):
+        messages.success(self.request, 'Blog was sucessfully deleted')
+        return reverse('blog:home')       
 
 
-    
+class UserPostListView(CheckRequestUser, ListView):
+    model = Post
+    template_name = 'users_blog.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        user_obj = self.request.user
+        posts = Post.objects.filter(author=user_obj).order_by('-id')
+        return posts 

8746fb9b9a09b8b2e24a319fd60590d4065edb66
Gaurav-dawadi <gaurabdawadi@gamil.com>
2020-12-25 21:33:33 +0545

    feat: add urls for added views

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, PostCreateView, PostUpdateView
+from .views import (UserRegistrationView, HomeView, PostDetailView, PostCreateView, PostUpdateView,
+                    PostDeleteView, UserPostListView)
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('register', UserRegistrationView.as_view(), name='register'),
     path('', HomeView.as_view(), name='home'),
     path('create', PostCreateView.as_view(), name='create'),
+    path('users-blog', UserPostListView.as_view(), name='users_blog'),
     path('<slug:slug>', PostDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', PostUpdateView.as_view(), name='update')
+    path('update/<int:pk>', PostUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', PostDeleteView.as_view(), name='delete'),
 ]

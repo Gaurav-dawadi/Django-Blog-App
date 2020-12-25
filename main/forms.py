930f91f2e9b6e040f94e4ef99891a62d9f42b29f
Gaurav-dawadi <gaurabdawadi@gamil.com>
2020-12-25 21:31:36 +0545

    feat: add user registration form

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class LoginForm(forms.Form):
@@ -10,9 +18,4 @@ class LoginForm(forms.Form):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'author', 'content']
-
-    def save(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            self.instance.author = self.request.user 
-        return super().save(*args, **kwargs)    
+        fields = ['title', 'content']   
